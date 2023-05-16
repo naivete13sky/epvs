aa2a67eb530d539dc0ab5045b382084ce9bf16bf
naivete13sky <chen320821@163.com>
2023-05-16 17:08:55 +0800

    20230516-6

--- config_ep/epcamTest.py
+++
@@ -1,9 +0,0 @@
-import os
-from epkernel import Configuration
-from epkernel import Input
-epcamPath = r"C:\cc\ep_local\product\EP-CAM\version\20230425\EP-CAM_release_1.1.3.18_jiami\Release"
-Configuration.init(epcamPath)
-Configuration.set_sysattr_path(os.path.join(epcamPath,r'config\attr_def\sysattr'))
-Configuration.set_userattr_path(os.path.join(epcamPath,r'config\attr_def\userattr'))
-result_each_file_identify = Input.file_identify(r"C:\Users\cheng.chen\Desktop\760\BOT_MASK.art")
-print(result_each_file_identify)
