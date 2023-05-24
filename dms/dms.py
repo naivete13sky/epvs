04bdf1f8de526c4f14cd3e557fa9e6c99de176df
naivete13sky <chen320821@163.com>
2023-05-24 15:34:35 +0800

    20230524-1 dms

--- dms/dms.py
+++ dms/dms.py
@@ -4,44 +4,52 @@ import pyppeteer
 import asyncio
 import time
 
+import gl as gl
 
 class DMS():
-
-
     def f1(self):
         pyppeteer.DEBUG = False
         loop = asyncio.get_event_loop()
-
         # print("*" * 30, "log in", "*" * 30)
-        task = asyncio.ensure_future(self.login('wangyufeng-wwyg', 'Cc123456*', 'http://10.97.80.119/admin/login/'))
-
+        task = asyncio.ensure_future(self.login('cc', 'cc', gl.url_login))
         print("*" * 30, "Begin log in DMS", "*" * 30)
-
         loop.run_until_complete(task)
-
-        # self.browser = task.result()
         print("*" * 30, "init finish", "*" * 30)
+
+        task = asyncio.ensure_future(self.job_test_add())
+        loop.run_until_complete(task)
 
     async def login(self, username, password, url):
         # 'headless': False如果想要浏览器隐藏更改False为True
-        self.browser = await launch({'headless': False, 'args': ['--no-sandbox']})
+        width, height = 1920, 1080  # 尺寸配置
+        self.browser = await launch({'headless': False,
+                                     'args': ['--disable-infobars',  # 关闭自动化提示框
+                                              '--no-sandbox',# 关闭沙盒模式
+                                              '--start-maximized',# 窗口最大化模式
+                                              ]})
         self.page = await self.browser.newPage()
+        await self.page.setViewport({'width': width, 'height': height})
         await self.page.setUserAgent(
             'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36')
+
+
+
+
+
+
         await self.page.goto(url)
-        # await self.page.type(gl.css_login_user, username)  # 输入用户名
-        # await self.page.type(gl.css_login_pwd, password)  # 输入密码
-        # # 获取验证码，先找到图片，再保存到本地
-        # html_content = await self.page.content()
-        # soup = BeautifulSoup(html_content, 'html.parser')
-        # mask_code_png_src = soup.find_all('img')[4].get('src')
-        # encode_img = mask_code_png_src.split(',')[1]
-        #
-        # await self.page.type(gl.css_login_mask_code, mask_code)
-        # await self.page.click('#app > div > div.login-panel > div > button')  # 点击登录
-        time.sleep(10)
+        await self.page.type(gl.css_login_user, username)  # 输入用户名
+        await self.page.type(gl.css_login_pwd, password)  # 输入密码
+        await self.page.click(gl.css_login_confirm)  # 点击登录
+        time.sleep(100)
         return self.page
 
+    async def job_test_add(self):
+        pass
+        print('job_test_add')
+        await asyncio.sleep(1)
+        await self.page.close()
 
-cc = DMS()
-cc.f1()
+
+dms = DMS()
+dms.f1()
---
+++ dms/gl.py
@@ -0,0 +1,10 @@
+url_login="http://10.97.80.119/admin/login/"
+
+
+css_login_user='#login-form > div:nth-child(2) > div > input'
+
+css_login_pwd='#login-form > div:nth-child(3) > div > input'
+
+css_login_confirm='#login-form > div.simpleui-input-inline.login-btn > button'
+
+css_job_test_menu = '#main > section > aside > ul > div > li.el-submenu.is-opened > ul > div > li:nth-child(1) > span'
