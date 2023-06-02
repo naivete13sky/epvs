67a0f7779f24fc2ace0135bc77232b359f669cc6
naivete13sky <chen320821@163.com>
2023-06-02 11:35:05 +0800

    20230602-1 dms,pyqt版本从5.9.1升级到了5.15.9，竟然还能启动epcam，我也不知道为什么 ok

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from logic.login import Login
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    app.setAttribute(Qt.AA_ShareOpenGLContexts)
+    app.setAttribute(Qt.AA_ShareOpenGLContexts)#启动浏览器需要先启动这个
     Ui_Login=Login()
     Ui_Login.show()#调用登录窗口
     sys.exit(app.exec_())
