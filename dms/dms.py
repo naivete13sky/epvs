f6c3ba645cfaf712ab2e48e472ff7125e1a17b1c
naivete13sky <chen320821@163.com>
2023-06-05 11:54:20 +0800

    20230605-4 上传主料号

--- dms/dms.py
+++ dms/dms.py
@@ -143,7 +143,59 @@ class DMS():
             # print('alert_tag_content:',alert_tag_content)
             return {'result':False,'info':alert_tag_content}
 
-    def add_main_job(self):
+    def add_main_job(self,job_name,has_file_type,status,from_object_pcb_factory,from_object_pcb_design,tags,remark):
+        from bs4 import BeautifulSoup
+
+        # 打开主料号页面，获取csrf
+        main_job_url = 'http://10.97.80.119/admin/job/job/add/'
+        response = self.session.get(main_job_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        input_tag = soup.find('input', {'name': 'csrfmiddlewaretoken'})
+        input_content = input_tag.get('value')
+        print('主料号新增页面csrf',input_content)
+
+        # 发送POST请求，录入主料号
+        # 文件路径
+        file_path = r"C:\Users\cheng.chen\Desktop\760.rar"
+
+        # 构建文件对象
+        files = {
+            'file_compressed': open(file_path, 'rb')  # 文件字段
+        }
+
+        post_data = {
+            'csrfmiddlewaretoken': input_content,
+            'job_name': job_name,
+            # 'file_compressed': ('760.rar', file_data,'application/octet-stream'),
+            'has_file_type': has_file_type,
+            'status':status,
+            'from_object_pcb_factory': from_object_pcb_factory,
+            'from_object_pcb_design': from_object_pcb_design,
+            'tags': tags,
+            'remark': remark,
+            '_save': '',
+            'actionName': 'actionValue',
+        }
+
+        # 构建请求头
+        headers = {
+            'Content-Type': 'application/octet-stream',
+        }
+
+
+        response = self.session.post(main_job_url, data=post_data,files=files)
+
+        print('status_code:',response.status_code)
+        print('response:',response.text)
+        # 检查登录是否成功
+        if response.status_code == 200:
+            print('登录成功！')
+            # 这个会话现在可以用于后续的请求，保持登录状态
+            # 在这里可以执行其他操作，如获取数据、访问其他页面等
+        else:
+            print('登录失败！')
+
+    def add_main_job0(self):
         import requests
         from bs4 import BeautifulSoup
 
@@ -199,9 +251,22 @@ class DMS():
         else:
             print('登录失败！')
 
+
+
+
 if __name__ == '__main__':
 
     dms = DMS()
-    # dms.f3()
     dms.login('cc','cc')
-    dms.add_main_job()
+
+    job_name = 'cctest7'
+    has_file_type = ['gerber274x', 'gerber274d', 'dxf', 'dwg']
+    status = 'draft'
+    from_object_pcb_factory = ''
+    from_object_pcb_design = ''
+    tags = 'test'
+    remark = '备注'
+
+    dms.add_main_job(job_name=job_name,has_file_type=has_file_type,status=status,
+                     from_object_pcb_factory=from_object_pcb_factory,from_object_pcb_design=from_object_pcb_factory,
+                     tags=tags,remark=remark)
