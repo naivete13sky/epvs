659f6841c996afce079356f469bb913e27020e9b
naivete13sky <chen320821@163.com>
2023-06-06 17:52:10 +0800

    20230606-6 上传比对结果到dms

--- dms/dms.py
+++ dms/dms.py
@@ -189,12 +189,12 @@ class DMS():
         print('status_code:',response.status_code)
         # print('response:',response.text)
         # 检查登录是否成功
-        if response.status_code == 200:
-            print('登录成功！')
-            # 这个会话现在可以用于后续的请求，保持登录状态
-            # 在这里可以执行其他操作，如获取数据、访问其他页面等
-        else:
-            print('登录失败！')
+        # if response.status_code == 200:
+        #     print('登录成功！')
+        #     # 这个会话现在可以用于后续的请求，保持登录状态
+        #     # 在这里可以执行其他操作，如获取数据、访问其他页面等
+        # else:
+        #     print('登录失败！')
 
     def add_main_job0(self):
         import requests
@@ -252,13 +252,67 @@ class DMS():
         else:
             print('登录失败！')
 
+    def add_test_job(self,job_parent,job_name,file_type,test_usage_for_epcam_module,vs_result_ep,vs_result_g,
+                     bug_info,bool_layer_info,vs_time_ep,vs_time_g,
+                     status,author,tags,remark,file_path_org,file_path_std):
+        from bs4 import BeautifulSoup
+
+        # 打开主料号页面，获取csrf
+        test_job_url = 'http://10.97.80.119/admin/eptest/jobfortest/add/'
+        response = self.session.get(test_job_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        input_tag = soup.find('input', {'name': 'csrfmiddlewaretoken'})
+        input_content = input_tag.get('value')
+        print('测试料号新增页面csrf',input_content)
+
+        # 发送POST请求，录入主料号
+        # 文件路径
+        file_path_org = file_path_org
+        file_path_std = file_path_std
+        # print('file_path:',file_path)
+
+        # 构建文件对象
+        files = {
+            'file': open(file_path_org, 'rb'),  # 文件字段
+            'standard_odb': open(file_path_std, 'rb')  # 文件字段
+        }
+
+        post_data = {
+            'csrfmiddlewaretoken': input_content,
+            'job_parent':job_parent,
+            'job_name': job_name,
+            'file_type': file_type,
+            'test_usage_for_epcam_module':test_usage_for_epcam_module,
+            'vs_result_ep':vs_result_ep,
+            'vs_result_g': vs_result_g,
+            'bug_info':bug_info,
+            'bool_layer_info':bool_layer_info,
+            'vs_time_ep':vs_time_ep,
+            'vs_time_g': vs_time_g,
+            'status':status,
+            'author': author,
+            'tags': tags,
+            'remark': remark,
+            '_save': '',
+            'actionName': 'actionValue',
+        }
+
+        # 构建请求头
+        headers = {
+            'Content-Type': 'application/octet-stream',
+        }
 
 
+        response = self.session.post(test_job_url, data=post_data,files=files)
 
-if __name__ == '__main__':
+        print('status_code:',response.status_code)
+        print('response:',response.text)
 
+
+def main_job_test():
+    pass
     dms = DMS()
-    dms.login('cc','cc')
+    dms.login('cc', 'cc')
 
     job_name = 'cctest7'
     has_file_type = ['gerber274x', 'gerber274d', 'dxf', 'dwg']
@@ -268,6 +322,51 @@ if __name__ == '__main__':
     tags = 'test'
     remark = '备注'
 
-    dms.add_main_job(job_name=job_name,has_file_type=has_file_type,status=status,
-                     from_object_pcb_factory=from_object_pcb_factory,from_object_pcb_design=from_object_pcb_factory,
-                     tags=tags,remark=remark)
+    dms.add_main_job(job_name=job_name, has_file_type=has_file_type, status=status,
+                     from_object_pcb_factory=from_object_pcb_factory, from_object_pcb_design=from_object_pcb_factory,
+                     tags=tags, remark=remark)
+
+def test_job_test():
+    pass
+    dms = DMS()
+    dms.login('cc', 'cc')
+
+    job_parent = '1773'
+    job_name = 'cctest7'
+    file_type = 'else'
+    test_usage_for_epcam_module = '8'
+    vs_result_ep = 'none'
+    vs_result_g = 'passed'
+    bug_info = ''
+    bool_layer_info = 'false'
+    vs_time_ep = ''
+    vs_time_g = ''
+    status = 'draft'
+    author = '1'
+    tags = 'test'
+    remark = '备注'
+    file_path_org = r"C:\Users\cheng.chen\Desktop\nca60led.rar"
+    file_path_std = r"C:\Users\cheng.chen\Desktop\nca60led.rar"
+
+    dms.add_test_job(job_parent=job_parent,
+                     job_name=job_name,
+                     file_type=file_type,
+                     test_usage_for_epcam_module=test_usage_for_epcam_module,
+                     vs_result_ep=vs_result_ep,
+                     vs_result_g=vs_result_g,
+                     bug_info=bug_info,
+                     bool_layer_info=bool_layer_info,
+                     vs_time_ep=vs_time_ep,
+                     vs_time_g=vs_time_g,
+                     status=status,
+                     author=author,
+                     tags=tags,
+                     remark=remark,
+                     file_path_org=file_path_org,
+                     file_path_std=file_path_std)
+
+if __name__ == '__main__':
+
+    pass
+    test_job_test()
+
