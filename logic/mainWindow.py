ba8d6deae0077e17ba2e5e57d4fe291bef1d9797
naivete13sky <chen320821@163.com>
2023-06-08 19:42:06 +0800

    20230608-13 因为EPCAM用的是QT5.9.1，所以只能只PyQt5.9.2，只能python3.7里安装。但是python3.7.9不支持QWebEngine。所以把浏览器功能注释掉了。不然打包后会有问题。

--- logic/mainWindow.py
+++ logic/mainWindow.py
@@ -294,18 +294,18 @@ class MainWindow(QMainWindow,Ui_MainWindow):
 
         # region 设置DMS初始页面
         # 创建一个新的标签页
-        self.tabMainDMS = QWidget()
-        from logic.webDMS import BrowserWindow
-        web_view_dms = BrowserWindow()
-        web_view_dms.load("http://10.97.80.119/admin/")  # 加载网页
-
-        # 在新标签页中添加布局并将浏览器窗口添加到布局中
-        web_view_dms_layout = QVBoxLayout()
-        web_view_dms_layout.addWidget(web_view_dms)
-        self.tabMainDMS.setLayout(web_view_dms_layout)
-
-        # 将新标签页添加到QTabWidget中
-        self.tabWidget.addTab(self.tabMainDMS, "料号管理")
+        # self.tabMainDMS = QWidget()
+        # from logic.webDMS import BrowserWindow
+        # web_view_dms = BrowserWindow()
+        # web_view_dms.load("http://10.97.80.119/admin/")  # 加载网页
+        #
+        # # 在新标签页中添加布局并将浏览器窗口添加到布局中
+        # web_view_dms_layout = QVBoxLayout()
+        # web_view_dms_layout.addWidget(web_view_dms)
+        # self.tabMainDMS.setLayout(web_view_dms_layout)
+        #
+        # # 将新标签页添加到QTabWidget中
+        # self.tabWidget.addTab(self.tabMainDMS, "料号管理")
         # endregion
 
 
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from logic.login import Login
 
 if __name__ == '__main__':
     import sys
-    QApplication.setAttribute(Qt.AA_ShareOpenGLContexts)#启动浏览器需要先启动这个
+    # QApplication.setAttribute(Qt.AA_ShareOpenGLContexts)#启动浏览器需要先启动这个
     app = QtWidgets.QApplication(sys.argv)
     # app.setAttribute(Qt.AA_ShareOpenGLContexts)#启动浏览器需要先启动这个
     Ui_Login=Login()
